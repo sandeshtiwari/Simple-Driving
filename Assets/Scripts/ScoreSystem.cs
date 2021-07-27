0958f4d6f1cf7e1c8019e1fe13591c746da154f8
stiwari64 <stiwari64@gatech.edu>
2021-07-26 23:13:07 -0500

    feat: Add high score saving

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -7,6 +7,7 @@ public class ScoreSystem : MonoBehaviour
 {
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier = 5f;
+    public const string HighScoreKey = "HighScore";
     private float score;
 
     // Update is called once per frame
@@ -15,5 +16,14 @@ public class ScoreSystem : MonoBehaviour
         score += Time.deltaTime * scoreMultiplier;
 
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HighScoreKey, Mathf.FloorToInt(score));
+        }
     }
 }
